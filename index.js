da008ac1ab7cecbd2aa744c5da21d19106703a37
steveruu <prchalek@gmail.com>
2023-01-22 12:50:41 +0100

    oops

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 // discord webhook url = https://discord.com/api/webhooks/1046402781696753715/DuBZxciO1pTDRHq3FnEa2EYsMjI1flapP5wsw_VTDNxUh05TPOPZA2TxJnkZJak8taQy
 const puppeteer = require('puppeteer');
 const axios = require('axios');
+require('dotenv').config();
 
 const artistList = [
   {id: '4NOFcRCgjvnRy8nKVGUM0L', name: 'Steveruu'},
-  {id: '1NspLfgAsucc39MeTipXNy', name: 'CYREX'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
-  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '1NspLfgAsucc39MeTipXNy', name: 'CYREX'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  // {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
 
 
 ];
-const discordWebhookUrl = 'https://discord.com/api/webhooks/1046402781696753715/DuBZxciO1pTDRHq3FnEa2EYsMjI1flapP5wsw_VTDNxUh05TPOPZA2TxJnkZJak8taQy';
+const discordWebhookUrl = process.env.DISCORD_WEBHOOK;
 
 (async () => {
   const browser = await puppeteer.launch();
@@ -45,7 +46,8 @@ const discordWebhookUrl = 'https://discord.com/api/webhooks/1046402781696753715/
   // Send the sorted artists data to Discord webhook
   for (const artist of artistList) {
     await axios.post(discordWebhookUrl, {
-      content: `${artist.name} has ${artist.listeners} monthly listeners`
+      // content: `${artist.name} has ${artist.listeners} monthly listeners`
+      content: `**tady steveruu, omlouvam se za tento incident, uz se to vickrat nestane :DDDDD**`
     })
     .then(response => {
       console.log('Data sent to Discord webhook.');
