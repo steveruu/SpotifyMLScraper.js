0743383700440f3492f160514f2e09e18bea677c
steveruu <prchalek@gmail.com>
2023-01-22 12:32:36 +0100

    YEE HAWWWW

---
+++ index.js
@@ -0,0 +1,60 @@
+// discord webhook url = https://discord.com/api/webhooks/1046402781696753715/DuBZxciO1pTDRHq3FnEa2EYsMjI1flapP5wsw_VTDNxUh05TPOPZA2TxJnkZJak8taQy
+const puppeteer = require('puppeteer');
+const axios = require('axios');
+
+const artistList = [
+  {id: '4NOFcRCgjvnRy8nKVGUM0L', name: 'Steveruu'},
+  {id: '1NspLfgAsucc39MeTipXNy', name: 'CYREX'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+  {id: '569eihmWcdg4HvSPDnjlPn', name: 'Ondrejoda'},
+
+
+];
+const discordWebhookUrl = 'https://discord.com/api/webhooks/1046402781696753715/DuBZxciO1pTDRHq3FnEa2EYsMjI1flapP5wsw_VTDNxUh05TPOPZA2TxJnkZJak8taQy';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  for (const artist of artistList) {
+    const url = `https://open.spotify.com/artist/${artist.id}`;
+    await page.goto(url);
+  
+    await page.waitForSelector('.Ydwa1P5GkCggtLlSvphs', { visible: true });
+  
+    const monthlyListeners = await page.evaluate(() => {
+      return document.querySelector('.Ydwa1P5GkCggtLlSvphs').textContent;
+    });
+  
+    artist.listeners = parseInt(monthlyListeners.replace(/[^0-9]/g, ''));
+  }
+  
+  artistList.sort((a, b) => {
+    return b.listeners - a.listeners;
+  });
+
+  // Send the sorted artists data to Discord webhook
+  for (const artist of artistList) {
+    await axios.post(discordWebhookUrl, {
+      content: `${artist.name} has ${artist.listeners} monthly listeners`
+    })
+    .then(response => {
+      console.log('Data sent to Discord webhook.');
+    })
+    .catch(error => {
+      console.log(`Error sending data to Discord webhook: ${error}`);
+    });
+  }
+
+  await browser.close();
+})();
+
