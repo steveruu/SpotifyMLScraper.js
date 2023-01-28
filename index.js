568e617b9ee3a656d6ae32e36288aaf841806cc2
steveruu <prchalek@gmail.com>
2023-01-28 22:45:36 +0100

    opravena docela zásadní chyba, lmao

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ const webhookURL = process.env.DISCORD_WEBHOOK;
     content: `update **${currentDate}:**`
   })
     .then(response => {
-      console.log(`Data ${content} sent to Discord webhook.`);
+      console.log(`Data sent to Discord webhook.`);
     })
     .catch(error => {
       console.log(`Error sending data to Discord webhook: ${error}`);
